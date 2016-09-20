5f38571592884c6c0b23feff943d17293f39f72c
Ijem <ij.ofili@gmail.com>
2016-09-20 02:35:29 +0100

    added entry

---
+++ hunibuniV1/platforms/browser/www/js/app.js
@@ -0,0 +1,13 @@
+//app.js
+var SCORE, MENU, QUIZ, TIMER, OPTIONS;
+
+
+//scoring: the player gets 1 point for each correct option selected
+
+//menu:
+
+//quiz: the question bank of the app. use an array to structure this??
+
+//timer: duration in which the the player MUST select a correct option
+
+//options: duration in which the the player MUST select a correct option
